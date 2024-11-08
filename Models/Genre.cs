3bd701fb6be3c9291329f69721fec73f7ff57a25
fouziMariem <mariem.elfouzi@insat.ucar.tn>
2024-11-08 12:08:21 +0100

    init

---
+++ Models/Genre.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,11 @@
+namespace Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Genre { get; set; }
+        public int Year { get; set; }
+    }
+    
+}
